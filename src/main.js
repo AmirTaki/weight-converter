d940d67490b4e6317e7be99214f298df0ba0c299
amir <amirtaki623@gmail.com>
2025-08-02 22:36:56 +0330

    function updateResults p1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 const inputEl = document.getElementById("input")
 const errorEl = document.getElementById("error")
 const resultEl = document.getElementById("result")
+
+let errorTime;
+let reusltTime;
+const updataResults = () => {
+    if(inputEl.value <= 0 || isNaN(inputEl.value)){
+
+    }
+    else {
+        
+    }
+}
